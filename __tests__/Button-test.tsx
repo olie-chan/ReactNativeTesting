4e1402c06930577c1d21300b2e0eb76e4faff8d6
Olie Chan <olie.chan@infinityworks.com>
2020-01-10 10:49:40 +0000

    Initial commit

---
+++ __tests__/Button-test.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {create, act} from 'react-test-renderer';
+import {Button, View} from 'react-native';
+
+function MyButton() {
+  const [someText, updateSomeText] = useState('');
+  return (
+    <View>
+      {someText}
+      <Button
+        title="Nothing to do for now"
+        onPress={() => updateSomeText('CLICKED')}
+      />
+    </View>
+  );
+}
+describe('Button component', () => {
+  test('it changes text when clicked', () => {
+		let component;
+		act(() => {
+			component = create(<MyButton />);
+		});
+		const instance = component.root;
+		const wrapper = instance.findByType('View');
+		let [ text, button ] = wrapper.children;
+
+		expect(text).toEqual('');
+
+		act(() => button.props.onPress());
+		text = wrapper.children[0];
+		expect(text).toEqual('CLICKED');
+  });
+});
