d200ecaef9a330656afa0cd56744c839fd33f976
Olie Chan <olie.chan@infinityworks.com>
2020-01-10 11:11:03 +0000

    Format code

--- __tests__/Button-test.tsx
+++ __tests__/Button-test.tsx
@@ -16,18 +16,22 @@ function MyButton() {
 }
 describe('Button component', () => {
   test('it changes text when clicked', () => {
-		let component;
-		act(() => {
-			component = create(<MyButton />);
-		});
-		const instance = component.root;
-		const wrapper = instance.findByType('View');
-		let [ text, button ] = wrapper.children;
+    let component;
+    act(() => {
+      component = create(<MyButton />);
+    });
+    if (!component) {
+      throw new Error('Component cannot be found');
+    }
 
-		expect(text).toEqual('');
+    const instance = component.root;
+    const wrapper = instance.findByType('View');
+    let [text, button] = wrapper.children;
 
-		act(() => button.props.onPress());
-		text = wrapper.children[0];
-		expect(text).toEqual('CLICKED');
+    expect(text).toEqual('');
+
+    act(() => button.props.onPress());
+    text = wrapper.children[0];
+    expect(text).toEqual('CLICKED');
   });
 });
